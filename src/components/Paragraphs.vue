6ce7d8f4973d5efd1a1a5a56ef890e53d3707966
TrenlinHuang <1149674138@qq.com>
2022-07-20 11:17:36 +0800

    paragraph.vue

---
+++ src/components/Paragraphs.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-card v-for="(c, i) in chapters" :key="i + c.header" elevation="0" width="100%">
+      <v-divider></v-divider>
+      <v-card-title>{{ c.header }}</v-card-title>
+      <v-card-text v-for="(p, j) in c.paragraphs" :key="i + c.header + j">
+        {{ p }}
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  props: {
+    chapters: Array
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
